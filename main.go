77f989cd64d9ddd5000f6686730983e30ed6a318
Russ Whelan <russell.whelan@gmail.com>
2020-02-28 16:00:16 +0000

    Add healthcheck

--- main.go
+++ main.go
@@ -10,7 +10,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there this is a microservice in Golang\n")
 }
 
+func healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/health", healthcheckHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
